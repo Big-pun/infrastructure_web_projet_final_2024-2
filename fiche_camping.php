2933ab2ab4e57980443fbe39b81fb7a945371f35
Big-pun <150970603+Big-pun@users.noreply.github.com>
2024-07-15 15:10:01 -0400

    fiche detaillée finie

--- fiche_camping.php
+++ fiche_camping.php
@@ -10,8 +10,11 @@ $camping = $resultat->fetch_assoc();
 ?>
 
 <!-- a-programmer -->
+<!-- Cette page doit être utilisée pour afficher la fiche détaillée du camping choisi par l'utilisateur -->
+<!-- Elle est volontairement vide, c'est à vous de la construire -->
+<!-- La mise en forme est à votre discrétion. -->
 
-<div> 
+<div>
         <img src="https://picsum.photos/id/<?= $_GET["id"] ?>/500/300" alt="<?= $camping['nom'] ?>">
         <h2><?= $camping['nom'] ?></h2>
         <p><?= $camping['region'] ?></p>
@@ -20,33 +23,12 @@ $camping = $resultat->fetch_assoc();
         <p><?= $camping['ville'] ?></p>
         <p><?= $camping['code_postal'] ?></p>
         <p><?= $camping['nb_etoiles'] ?> étoiles</p>
-        <p><?= $camping['accepte_animaux'] ? "Animaux acceptés" : "Animaux non acceptés" ?></p>
+        <p>Annimaux accéptés : <?= $camping['accepte_animaux'] ? "Oui" : "Non" ?></p>
 </div>
-
- 
-
-<!-- Cette page doit être utilisée pour afficher la fiche détaillée du camping choisi par l'utilisateur -->
-<!-- Elle est volontairement vide, c'est à vous de la construire -->
-
-<!-- La mise en forme est à votre discrétion. -->
-
-<!-- Les informations à afficher sont 
-        - l’image (statique ou dynamique selon le # picsum)
-        - le nom du camping
-        - la région
-        - la description
-        - l'adresse complète (adresse, ville et code postal)
-        - le nombre d’étoiles
-        - le nombre de terrains
-        - la mention « animaux acceptés : » suivie de « oui » ou « non ». 
--->
-
-<!-- Assurez-vous que la page affiche l'entête et le pied de page, comme les autres pages -->
-<!-- et que tout est valide W3C -->
 
 <main>
         <div>
-                
+
         </div>
 </main>
 
